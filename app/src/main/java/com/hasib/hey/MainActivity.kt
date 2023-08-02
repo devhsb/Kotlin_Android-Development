4ab0519fd4dbf14a64f2cdffe943222e3dd984c6
hasib <hasibpanjshery2@gmail.com>
2023-08-02 20:29:52 +0500

    image added

--- app/src/main/java/com/hasib/hey/MainActivity.kt
+++ app/src/main/java/com/hasib/hey/MainActivity.kt
@@ -3,7 +3,9 @@ package com.hasib.hey
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,6 +15,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -43,30 +49,38 @@ fun MyName(modifier: Modifier = Modifier) {
         modifier = modifier
     ){
         Text(
-            text = "HaSeeb",
-            lineHeight = 70.sp,
+            text = stringResource(R.string.my_name),
+            lineHeight = 40.sp,
             fontSize = 70.sp,
             textAlign = TextAlign.Center,
             modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
         )
         Text(
-            text = "Software Developer",
+            text = stringResource(R.string.my_field),
             modifier = Modifier
                 .padding(20.dp)
-                .align(alignment = Alignment.End)
+                .align(alignment = Alignment.CenterHorizontally),
+            color = Color.White,
+            fontSize = 20.sp
         )
     }
-
 }
 
 @Preview(
-    showBackground = true,
     showSystemUi = true,
-    name = "Preview My Name"
+    name = "Show result"
 )
 @Composable
-fun MyNamePreview(){
-    HeyTheme {
-        MyName()
+fun BackgroundImage(modifier: Modifier = Modifier) {
+    Box(modifier) {
+        Image(
+            painter = painterResource(id = R.drawable.wallpaper_back),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.7F
+        )
+        MyName(
+            modifier = Modifier.fillMaxSize()
+        )
     }
 }
