233ab52b21e946d3d12d6be778abecc4d4c5905b
HaSeeb <hasibpanjshery2@gmail.com>
2023-08-16 00:50:47 +0500

    ui improvment

--- 08 - Woof App/app/src/main/java/com/hasib/woof/MainActivity.kt
+++ 08 - Woof App/app/src/main/java/com/hasib/woof/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.hasib.woof
 
+import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,7 +18,11 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,8 +32,10 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.hasib.woof.data.Dogs
 import com.hasib.woof.model.Dog
 import com.hasib.woof.ui.theme.WoofTheme
@@ -42,7 +50,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    WoofApp()
                 }
             }
         }
@@ -51,19 +59,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WoofApp() {
-    
+    DogsList(dogs = Dogs().getDogsList())
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DogsList(dogs: List<Dog>) {
-    LazyColumn {
-        items(dogs) { dog ->
+
+    Scaffold(
+        topBar = {
+            WoofTopAppbar()
+        }
+    ) { it ->
+        LazyColumn(contentPadding = it) {
+            items(dogs) { dog ->
                 DogCard(
                     dog = dog,
-                    modifier = Modifier.padding(top = dimensionResource(id = R.dimen.largePadding),
+                    modifier = Modifier.padding(
+                        top = dimensionResource(id = R.dimen.largePadding),
                         start = dimensionResource(id = R.dimen.MediumPadding),
-                        end = dimensionResource(id = R.dimen.MediumPadding))
+                        end = dimensionResource(id = R.dimen.MediumPadding)
+                    )
                 )
+            }
         }
     }
 }
@@ -73,7 +91,8 @@ fun DogCard(modifier: Modifier = Modifier,
             dog: Dog
 ) {
     Card(
-        modifier = modifier
+        modifier = modifier,
+        shape = RoundedCornerShape(bottomStart = 16.dp, topEnd = 16.dp)
     ) {
         Row(
             modifier = Modifier.fillMaxWidth(),
@@ -93,10 +112,13 @@ fun DogCard(modifier: Modifier = Modifier,
             ) {
                 Text(
                     text = dog.dogName,
-                    style = MaterialTheme.typography.headlineMedium)
+                    style = MaterialTheme.typography.displayLarge,
+                    letterSpacing = 2.sp
+                )
                 Text(
                     text = "${dog.dogAge} years old",
-                    style = MaterialTheme.typography.labelLarge
+                    style = MaterialTheme.typography.labelLarge,
+                    letterSpacing = 1.sp
                 )
             }
         }
@@ -104,15 +126,33 @@ fun DogCard(modifier: Modifier = Modifier,
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WoofTopAppbar(modifier: Modifier = Modifier) {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.app_name),
+                        style = MaterialTheme.typography.displayMedium,
+                        fontSize = 40.sp
+                    )
+                }
+            )
+}
+
+
+//@Preview()
 @Composable
 fun CardPreview() {
     DogCard(dog = Dogs().getDogsList().get(2))
 }
 
-@Preview()
+//@Preview(showSystemUi = true, showBackground = true)
 @Composable
 fun ListPreview() {
-    DogsList(dogs = Dogs().getDogsList())
+    WoofTheme(useDarkTheme = true) {
+        DogsList(dogs = Dogs().getDogsList())
+    }
 }
 
 
--- 08 - Woof App/app/src/main/java/com/hasib/woof/ui/theme/Type.kt
+++ 08 - Woof App/app/src/main/java/com/hasib/woof/ui/theme/Type.kt
@@ -2,33 +2,31 @@ package com.hasib.woof.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.hasib.woof.R
 
 // Set of Material typography styles to start with
+val Vampire_wars = FontFamily(
+    Font(R.font.azonix)
+)
+val Azonix = FontFamily(
+    Font(R.font.vampire_wars, FontWeight.Bold)
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    displayLarge = TextStyle(
+        fontFamily = Vampire_wars,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+
+    displayMedium = TextStyle(
+        fontFamily = Azonix,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
     )
-    */
 )
